3744c2402d70d82108386b4a08953eaa1579c6f6
humkyung <zbaekhk@gmail.com>
2017-04-04 11:18:54 +0900

    python file 추가

---
+++ pyPDF.py
@@ -0,0 +1,11 @@
+import pyPDF
+from pyPdf import PdfFileReader
+
+def printMeta(fileName):
+    pdfFile = PdfFileReader(file(fileName, 'rb'))
+    docInfo = pdfFile.getDocumentInfo()
+    print '[*] PDF MetaData For: ' + str(fileName)
+    for metaItem in docInfo:
+        print '[+] ' + metaItem + ':' + docInfo[metaItem]
+
+printMeta()
